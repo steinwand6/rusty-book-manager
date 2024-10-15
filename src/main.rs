8abc699469db19527a682b9d2c5ecd6f4e33455a
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-15 20:48:11 +0900

    chore: improve error handling with anyhow and update health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
+use anyhow::Result;
 use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/health_check", get(health_check));
+async fn main() -> Result<()> {
+    let app = Router::new().route("/health", get(health_check));
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
 
     println!("Listening on {addr}");
 
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
 
 async fn health_check() -> StatusCode {
