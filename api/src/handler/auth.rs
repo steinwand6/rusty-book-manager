946d957d137ab9d225fa8b3a0f5bb29d06873b16
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-12-21 18:54:41 +0900

    feat: add logout handler

--- api/src/handler/auth.rs
+++ api/src/handler/auth.rs
@@ -1,9 +1,12 @@
-use axum::{extract::State, Json};
+use axum::{extract::State, http::StatusCode, Json};
 use kernel::model::auth::event::CreateToken;
 use registry::AppRegistory;
 use shared::error::AppResult;
 
-use crate::model::auth::{AccessTokenResponse, LoginRequest};
+use crate::{
+    extractor::AuthorizedUser,
+    model::auth::{AccessTokenResponse, LoginRequest},
+};
 
 pub async fn login(
     State(registry): State<AppRegistory>,
@@ -24,6 +27,13 @@ pub async fn login(
     }))
 }
 
-pub async fn logout(State(registry): State<AppRegistory>) {
-    todo!()
+pub async fn logout(
+    user: AuthorizedUser,
+    State(registry): State<AppRegistory>,
+) -> AppResult<StatusCode> {
+    registry
+        .auth_repository()
+        .delete_token(user.access_token)
+        .await?;
+    Ok(StatusCode::NO_CONTENT)
 }
